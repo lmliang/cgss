d53408c57f6236fc7c86fbc9f670186006cd83e8
Li Mingliang <lmliang@live.cn>
2014-04-14 17:17:59 +0800

    add centerclient.go file.

---
+++ src/cg/centerclient.go
@@ -0,0 +1,59 @@
+package cg
+
+import (
+	"encoding/json"
+	"errors"
+
+	"ipc"
+)
+
+type CenterClient struct {
+	*ipc.IpcClient
+}
+
+func (client *CenterClient) AddPlayer(player *Player) error {
+	b, err := json.Marshal(player)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Call("addplayer", string(b))
+	if err == nil && resp.Code == "200" {
+		return nil
+	}
+	return err
+}
+
+func (client *CenterClient) RemovePlayer(name string) error {
+	resp := client.Call("removeplayer", name)
+	if resp.Code == "200" {
+		return nil
+	}
+	return errors.New(resp.Code)
+}
+
+func (client *CenterClient) ListPlayer() (ps []*Player, err error) {
+	resp := client.Call("listplayer", "")
+	if resp.Code != "200" {
+		err = errors.New(resp.Code)
+		return
+	}
+
+	err = json.Unmarshal([]byte(resp.Body), &ps)
+	return
+}
+
+func (client *CenterClient) Broadcast(message string) error {
+	var m = &Message{content: message}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	resp, _ := client.Call("broadcast", string(b))
+	if resp.Code == "200" {
+		return nil
+	}
+	return errors.New(resp.Code)
+}
